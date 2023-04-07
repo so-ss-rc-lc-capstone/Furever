d351af3f709ea721a35cd4f19ba449923d5cd2dc
Songju Oh <songju.oh1@gmail.com>
2023-04-07 13:08:26 -0500

    data table name changed petprofile -> pet

---
+++ src/main/java/com/codeup/codeupspringblog/models/Pet.java
@@ -0,0 +1,65 @@
+package com.codeup.codeupspringblog.models;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name="pets")
+public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false, length=50)
+    private String name;
+    @Column(nullable = false)
+    private Date dateOfBirth;
+    @Column(nullable = true)
+    private String image;
+    @Column(nullable = true, columnDefinition = "Enum('F', 'M')")
+    private String gender;
+
+
+    public Pet() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
