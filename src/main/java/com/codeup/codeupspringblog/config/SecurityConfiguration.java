de7f1dae4196f82338b39e074fca5c3eb85048fa
Randy Chu <randychu7@gmail.com>
2023-04-10 14:39:18 -0500

    styling

--- src/main/java/com/codeup/codeupspringblog/config/SecurityConfiguration.java
+++ src/main/java/com/codeup/codeupspringblog/config/SecurityConfiguration.java
@@ -37,7 +37,7 @@ public class SecurityConfiguration {
                 /* Login configuration */
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/profile") // user's home page, it can be any URL
+                .defaultSuccessUrl("/events") // user's home page, it can be any URL
                 .permitAll() // Anyone can go to the login page
                 /* Logout configuration */
                 .and()
