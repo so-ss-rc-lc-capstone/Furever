357b412357179c132f6fc8941b6cd595e7cf7db7
Luke Calderon <luke.a.calderon@gmail.com>
2023-04-11 11:25:21 -0500

    saves value input

--- src/main/resources/static/js/weather_map.js
+++ src/main/resources/static/js/weather_map.js
@@ -3,6 +3,7 @@ let userLocation = {
     longitude: 0,
     latitude: 0
 };
+let destinations = [];
 
 navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {enableHighAccuracy: true});
 
@@ -10,7 +11,10 @@ function successLocation(position) {
     userLocation.longitude = position.coords.longitude;
     userLocation.latitude = position.coords.latitude;
     setupMap([userLocation.longitude, userLocation.latitude]);
+    // store user location in an array
+    destinations.push([userLocation.longitude, userLocation.latitude]);
 }
+
 
 function errorLocation() {
     setupMap([-98.4916, 29.4252]);
@@ -19,7 +23,8 @@ function errorLocation() {
 function setupMap(center) {
     const map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/lukecal24/clg5hkzhf009x01mnuqxbp3y6',
+        // style: 'mapbox://styles/lukecal24/clg5hkzhf009x01mnuqxbp3y6',
+        style: 'mapbox://styles/mapbox/dark-v11',
         center: center,
         zoom: 10
     });
@@ -31,8 +36,21 @@ function setupMap(center) {
         accessToken: mapboxgl.accessToken
     });
 
+
     map.addControl(directions, "top-left");
 
-    // Example of how to access the user's location
-    console.log("User's location: ", userLocation.longitude, userLocation.latitude);
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const origin = document.querySelector('#mapbox-directions-origin-input input').value;
+        const destination = document.querySelector('#mapbox-directions-destination-input input').value;
+        destinations.push(origin, destination);
+    });
+
 }
+console.log(userLocation);
+console.log(destinations);
+
+
+
+//the destinations array in the console is only showing the user location not the 2 inputs . the id of the inputs are id="mapbox-directions-origin-input" and id="mapbox-directions-destination-input"
