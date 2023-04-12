b5a4e11b7b6863b9cc401c1988496331d257efa5
Luke Calderon <luke.a.calderon@gmail.com>
2023-04-12 14:35:48 -0500

    added end point to security configuration test controller holds the method to convert users to json

--- src/main/java/com/codeup/codeupspringblog/config/SecurityConfiguration.java
+++ src/main/java/com/codeup/codeupspringblog/config/SecurityConfiguration.java
@@ -34,7 +34,6 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-
                 /* Login configuration */
                 .formLogin()
                 .loginPage("/login")
