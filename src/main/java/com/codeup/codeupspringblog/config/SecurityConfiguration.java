338d454051bc259e647d7134c7ad77e3c783a68f
Songju Oh <songju.oh1@gmail.com>
2023-04-12 11:27:02 -0500

    worked on multiple area including: security, event image reder

--- src/main/java/com/codeup/codeupspringblog/config/SecurityConfiguration.java
+++ src/main/java/com/codeup/codeupspringblog/config/SecurityConfiguration.java
@@ -34,7 +34,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+//                .csrf().disable()
 
                 /* Login configuration */
                 .formLogin()
