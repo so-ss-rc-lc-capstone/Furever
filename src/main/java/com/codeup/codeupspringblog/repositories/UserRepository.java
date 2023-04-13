ec558a9305facf7aef901865cddf537d4e78e3cc
Luke Calderon <luke.a.calderon@gmail.com>
2023-04-13 13:54:35 -0500

    removed uneeded changes from user repository

--- src/main/java/com/codeup/codeupspringblog/repositories/UserRepository.java
+++ src/main/java/com/codeup/codeupspringblog/repositories/UserRepository.java
@@ -5,14 +5,10 @@ import com.codeup.codeupspringblog.models.Post;
 import com.codeup.codeupspringblog.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findById(long id);
 
     User findByUsername(String username);
-
-    //for search bar
-    List<User> findByUsernameContainingIgnoreCaseOrEmailContainingIgnoreCase(String username, String email);
 }
