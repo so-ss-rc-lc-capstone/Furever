fb0aefbc61b5bb67ba0b9dcdc36eede3b791d504
Luke Calderon <luke.a.calderon@gmail.com>
2023-04-14 14:47:29 -0500

    working on re directing

--- src/main/resources/static/js/custom-map-stuff.js
+++ src/main/resources/static/js/custom-map-stuff.js
@@ -22,6 +22,7 @@ fetch('http://localhost:8080/api/allevents', {
     .then(response => response.json())
     .then(data => {
         const event = data.map(event => ({
+            id: event.id,
             title: event.title,
             name: event.location_name,
             address: event.location_address
@@ -36,6 +37,13 @@ fetch('http://localhost:8080/api/allevents', {
             center: [-98.4916, 29.4252]
         });
 
+        // Get the user's current position
+        navigator.geolocation.getCurrentPosition(position => {
+            // Update the map's center to the user's position
+            map.setCenter([position.coords.longitude, position.coords.latitude]);
+            map.setZoom(10);
+        });
+
         event.forEach(function(event) {
             geocode(event.address, keys.mapbox).then(function (result) {
                 console.log(result[0]);
@@ -46,10 +54,20 @@ fetch('http://localhost:8080/api/allevents', {
                     .addTo(map);
 
                 let eventPopup = new mapboxgl.Popup()
-                    .setHTML(`<h2>Event Name: ${event.title}</h2><h3>Location: ${event.name}</h3><p>Address: ${event.address}</p>`);
+                    .setHTML(`<h2>Event Name: ${event.title}</h2><h3>Location: ${event.name}</h3><p>Address: ${event.address}</p>`)
+                    .on('click', function(e) {
+                        // Prevent the default map click behavior
+                        e.preventDefault();
 
-                // map.setCenter(result);
-                map.setZoom(10);
+                        // Extract the eventId from the event object
+                        let eventId = event.id;
+
+                        // Construct the URL for the event details page
+                        let url = `http://localhost:8080/events/${eventId}/find?event=${eventId}`;
+
+                        // Open the event details page in a new tab
+                        window.open(url, '_blank');
+                    });
 
                 marker.setPopup(eventPopup);
             });
@@ -57,34 +75,5 @@ fetch('http://localhost:8080/api/allevents', {
     })
     .catch(error => console.error(error));
 
-
-// mapboxgl.accessToken = keys.mapbox;
-// let map = new mapboxgl.Map({
-//     container: 'map',
-//     // style: 'mapbox://styles/lukecal24/clg5hkzhf009x01mnuqxbp3y6',
-//     style: 'mapbox://styles/mapbox/dark-v11',
-//     zoom: 13,
-//     center: [-98.4916, 29.4252]
-// });
-
-
-// eventLoc.forEach(function(event) {
-//     geocode(event.location_address, keys.mapbox).then(function (result) {
-//         console.log(result[0]);
-//         console.log(result[1]);
-//
-//         let marker = new mapboxgl.Marker()
-//             .setLngLat([result[0],result[1]])
-//             .addTo(map);
-//
-//         let eventPopup = new mapboxgl.Popup()
-//             .setHTML(`<h3>${event.location_name}</h3><p>${event.location_address}</p>`);
-//
-//         // map.setCenter(result);
-//         map.setZoom(10);
-//
-//         marker.setPopup(eventPopup);
-//     });
-// })
 
 
