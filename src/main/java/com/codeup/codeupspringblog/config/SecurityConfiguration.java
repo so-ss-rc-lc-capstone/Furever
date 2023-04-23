bbd251a69e5143e1a8291ed8b80e868252e8c4e6
Randy Chu <randychu7@gmail.com>
2023-04-22 19:04:20 -0500

    made changes to home page

--- src/main/java/com/codeup/codeupspringblog/config/SecurityConfiguration.java
+++ src/main/java/com/codeup/codeupspringblog/config/SecurityConfiguration.java
@@ -58,7 +58,6 @@ public class SecurityConfiguration {
                         "/posts/index",
                         "/posts/{id}/like",
 
-                        "/matrix",
                         "/comment/create",
                         "/login",
                         "/logout",
