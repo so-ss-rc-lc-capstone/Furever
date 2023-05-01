9416ff976d441ca9d919376f5091a1a129cbfc8f
Randy Chu <randychu7@gmail.com>
2023-05-01 11:42:57 -0500

    ok

--- src/main/resources/static/js/darktoggle.js
+++ src/main/resources/static/js/darktoggle.js
@@ -1,19 +1,22 @@
 
 // script.js
+// Add click event listener to your toggle button
 
-const button = document.getElementById('darkmode');
-const body = document.querySelector('body');
 
-button.addEventListener('click', () => {
-    body.classList.toggle('dark');
-    if (body.classList.contains('dark')) {
-        localStorage.setItem('darkMode', 'true');
-    } else {
-        localStorage.removeItem('darkMode');
-    }
-});
-
-// Check if dark mode is enabled on page load
-if (localStorage.getItem('darkMode') === 'true') {
-    body.classList.add('dark');
-}
+//
+// const button = document.getElementById('darkmode');
+// const body = document.querySelector('body');
+//
+// button.addEventListener('click', () => {
+//     body.classList.toggle('dark');
+//     if (body.classList.contains('dark')) {
+//         localStorage.setItem('darkMode', 'true');
+//     } else {
+//         localStorage.removeItem('darkMode');
+//     }
+// });
+//
+// // Check if dark mode is enabled on page load
+// if (localStorage.getItem('darkMode') === 'true') {
+//     body.classList.add('dark');
+// }
